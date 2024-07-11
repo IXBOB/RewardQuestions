17b4e696a0581102b6a85507c38f3b79f6391670
IXBOB <tangqiyu17@gmail.com>
2024-07-12 07:20:09 +0800

    added tip to console when console typed /quesreload

--- src/main/java/com/ixbob/rewardquestions/command/QuesreloadCommand.java
+++ src/main/java/com/ixbob/rewardquestions/command/QuesreloadCommand.java
@@ -1,6 +1,7 @@
 package com.ixbob.rewardquestions.command;
 
 import com.ixbob.rewardquestions.Main;
+import net.kyori.adventure.text.Component;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,13 +12,15 @@ import org.jetbrains.annotations.NotNull;
 public class QuesreloadCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
-        if (commandSender instanceof Player player) {
+        if (commandSender instanceof ConsoleCommandSender console) {
+            Main.getInstance().reloadConfig();
+            console.sendMessage(Component.text("§a已重新加载插件"));
+        }
+        else if (commandSender instanceof Player player) {
             if (player.isOp()) {
                 Main.getInstance().reloadConfig();
-                player.sendMessage("§a已重新加载插件");
+                player.sendMessage(Component.text("§a已重新加载插件"));
             }
-        } else if (commandSender instanceof ConsoleCommandSender) {
-            Main.getInstance().reloadConfig();
         }
         return true;
     }
